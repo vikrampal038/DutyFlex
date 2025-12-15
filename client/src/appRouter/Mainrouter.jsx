43eeb99d571cf5c35a36b9c5ca78bccf16d37802
vikrampal038 <vikrampal038@gmail.com>
2025-12-15 18:27:11 +0530

    Update All Pages

--- client/src/appRouter/Mainrouter.jsx
+++ client/src/appRouter/Mainrouter.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import {Routes, Route} from 'react-router-dom'
 import Home from '../pages/Home'
-// import Platform from '../pages/Platform'
-// import Solutions from '../pages/Solutions'
-// import Resources from '../pages/Resources'
-// import Pricing from '../pages/Pricing'
+import Platform from '../pages/Platform'
+import Solutions from '../pages/Solutions'
+import Resources from '../pages/Resources'
+import Pricing from '../pages/Pricing'
 import Accounts from '../components/Accounts/Accounts'
 import FeaturesDetails from '../pages/FeaturesDetails'
 
@@ -14,10 +14,10 @@ const Mainrouter = () => {
       <div>
         <Routes>
           <Route path='/' element={<Home />} />
-          {/* <Route path='/platform' element={<Platform />} />
+          <Route path='/platform' element={<Platform />} />
           <Route path='/solutions' element={<Solutions />} />
           <Route path='/resources' element={<Resources />} />
-          <Route path='/pricing' element={<Pricing />} /> */}
+          <Route path='/pricing' element={<Pricing />} /> 
           <Route path='/accounts/:page' element={<Accounts />} />
           <Route path='/detail/:id' element={<FeaturesDetails />} />
           
